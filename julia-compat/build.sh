5e241b9449fe4122fc2975d45286c394fe150f82
Dan Fornika <dfornika@gmail.com>
2017-07-24 15:37:29 -0700

    Update

--- julia-compat/build.sh
+++ julia-compat/build.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+if [ "$JULIA_VERSION" = "<UNDEFINED>" -o "$JULIA_VERSION" = "" ]
+then
+    JULIA_VERSION=0.5
+fi
+
 JULIA_PKG_NAME=Compat
 DEST="$PREFIX/share/julia/site/v$JULIA_VERSION/$JULIA_PKG_NAME"
 
