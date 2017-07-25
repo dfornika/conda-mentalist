98dc549f685c7611be7fc8092dcba5a9d60492a0
Dan Fornika <dfornika@gmail.com>
2017-07-24 19:18:33 -0700

    Removed tests

--- julia-compat/build.sh
+++ julia-compat/build.sh
@@ -9,4 +9,5 @@ JULIA_PKG_NAME=Compat
 DEST="$PREFIX/share/julia/site/v$JULIA_VERSION/$JULIA_PKG_NAME"
 
 mkdir -p "$DEST"
-cp -a "$PWD" "$DEST"
+cp --recursive --archive --no-target-directory "$PWD" "$DEST"
+
