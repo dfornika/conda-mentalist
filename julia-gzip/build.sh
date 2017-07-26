3b5ea7240289280de28be0095b74b2f0ed470189
Dan Fornika <dfornika@gmail.com>
2017-07-25 17:05:25 -0700

    Added 7 new packages

---
+++ julia-gzip/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$JULIA_VERSION" = "<UNDEFINED>" -o "$JULIA_VERSION" = "" ]
+then
+    JULIA_VERSION=0.5
+fi
+
+JULIA_PKG_NAME=GZip
+DEST="$PREFIX/share/julia/site/v$JULIA_VERSION/$JULIA_PKG_NAME"
+
+mkdir -p "$DEST"
+cp --recursive --archive --no-target-directory "$PWD" "$DEST"
+
