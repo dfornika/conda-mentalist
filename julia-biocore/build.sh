90042e0c2e812369c0c7647ffc4dc7b8d0c984fe
Dan Fornika <dfornika@gmail.com>
2017-08-28 13:15:50 -0700

    Added several packages

---
+++ julia-biocore/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$JULIA_VERSION" = "<UNDEFINED>" -o "$JULIA_VERSION" = "" ]
+then
+    JULIA_VERSION=0.5
+fi
+
+JULIA_PKG_NAME=BioCore
+DEST="$PREFIX/share/julia/site/v$JULIA_VERSION/$JULIA_PKG_NAME"
+
+mkdir -p "$DEST"
+cp --recursive --archive --no-target-directory "$PWD" "$DEST"
+
